d916f36ae70fbd2ead8e2a2cd1b5ed895532f909
Milos Mirkovic <milosdmirkovic@gmai.com>
2026-02-11 20:55:53 +0100

    add PracticeFunctions contract with increment, double, reset and memory example

---
+++ contracts/PracticeFunctions.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract PracticeFunctions {
+    uint256 public counter;
+    address public owner;
+    uint256[] public numbers;
+
+    constructor() {
+        owner = msg.sender;
+        counter = 0;
+    }
+
+    function getCounter() public view returns (uint256) {
+        return counter;
+    }
+
+    function increment() external {
+        counter += 1;
+    }
+
+    function doubleCounter() external {
+        counter *= 2;
+    }
+
+    function resetCounter() external {
+        counter = 0;
+    }
+
+    function addMemoryExample() public pure returns(uint256[] memory){
+    uint256[] memory temp = new uint256[](3);
+    temp[0]=1;
+    temp[1]=2;
+    temp[2]=3;
+
+    return temp;
+    }
+}
