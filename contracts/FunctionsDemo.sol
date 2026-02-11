c6387316aa71bfa320cd631c367e445cbcd71be4
Milos Mirkovic <milosdmirkovic@gmai.com>
2026-02-11 20:42:45 +0100

    init FunctionsDemo contract with counter and owner

--- contracts/FunctionsDemo.sol
+++ contracts/FunctionsDemo.sol
@@ -2,16 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract FunctionsDemo {
-    // DAY 37: Functions - Logic & Gas 
-    uint256 public counter; // State variables (stored on blockchain - cost gas to write!)
-    address public owner; 
-    uint256[] public numbers; // Storage array
+    uint256 public counter;
+    address public owner;
 
-    constructor(){
+    constructor() {
         owner = msg.sender;
         counter = 0;
     }
-    // VIEW function - reads state, doesn't modify (FREE to call externally!)
+
     function getCounter() public view returns (uint256) {
         return counter;
     }
@@ -19,19 +17,4 @@ contract FunctionsDemo {
     function increment() external {
         counter += 1;
     }
-    function addMemoryExample() public pure returns (uint256[] memory){
-    
-    uint256[] memory temp = new uint256[](3);
-
-    temp[0] = 1;
-    temp[1] = 2;
-    temp[2] = 3;
-
-    return temp;
 }
-    function doubleCounter() external {
-      counter = counter * 2;
-}
-
-
- }
